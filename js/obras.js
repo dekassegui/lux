5600ee5765b2c4245fe6ba2a9e88a0e664598191
dekassegui <dekassegui@gmail.com>
2017-02-07 02:07:45 -0200

    Ajuste de conteúdo local e remoto.

--- js/obras.js
+++ js/obras.js
@@ -52,49 +52,7 @@ window.addEventListener('load',
 
     var commandButtons = [updateBtn, delBtn, searchBtn, newBtn];
 
-    var MURAL = (function () {
-
-      var mural = $('mural');  // área de notificações ao usuário
-
-      var lineHeight = parseInt(getCSSproperty(mural, 'line-height'));
-
-      mural.oninput = function () {
-        if (mural.textLength == 0) {
-          mural.classList.add('empty');
-        } else {
-          mural.classList.remove('empty');
-        }
-      };
-
-      // agrega 'text' como apêndice do conteúdo da textarea cujo canvas
-      // escorre até que 'text' seja visível tão ao topo quanto possível
-      this.append = function (text) {
-        if (text.map) {
-          text.map(append);
-        } else {
-          var a = mural.clientHeight,   // altura do canvas
-              b = mural.scrollHeight;   // altura do conteúdo a priori
-          if (mural.textLength > 0) {
-            mural.value = [mural.value, text].join("\n");
-          } else {
-            mural.value = text;
-            mural.oninput();
-          }
-          if (b > a) {
-            mural.scrollTop = b - lineHeight;
-          }
-        }
-      };
-
-      this.isEmpty = function() { return mural.textLength == 0; };
-
-      // inicia o mural com saudação em função da hora local
-      mural.value = ["> Boa noite!", "> Bom dia!", "> Boa tarde!"]
-        [Math.floor(new Date().getHours() / 6) % 3];
-
-      return this;
-
-    })();
+    var MURAL = new Mural();
 
     function print(text) { MURAL.append(text); }
 
