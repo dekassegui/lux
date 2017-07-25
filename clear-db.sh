171b9735bdc6355c3317e843e8281e2a0bb6b53c
dekassegui <dekassegui@gmail.com>
2017-07-25 17:14:49 -0300

    Correção do script de manutenção do DB.

--- clear-db.sh
+++ clear-db.sh
@@ -3,7 +3,14 @@
 # Higienização do db do LUX se houver ao menos um registro de empréstimo
 # com data de devolução não nula, preservando arquivos num zipfile.
 #
-pushd /home/sergio/Projects/lux
+# pesquisa diretório do LUX
+luxdir=$(find /home/$USER -type d -user $USER -perm -0755 -name lux -print)
+if [[ -z $luxdir ]]; then
+  echo 'Não localizou o diretório do LUX.';
+  exit 1
+fi
+# muda para o diretório do LUX se necessário
+[[ $PWD == $luxdir ]] && unset luxdir || pushd $luxdir
 dbfile='datum/lux.sqlite'
 read -d '' sql <<EOT
  SELECT EXISTS(SELECT 1 FROM emprestimos WHERE data_devolucao NOTNULL LIMIT 1)
@@ -19,4 +26,4 @@ if [[ $(sqlite3 $dbfile "$sql") == 1 ]]; then
   # exclusão dos registros da tabela "emprestimos" com data_devolucao não nula
   sudo sqlite3 $dbfile '.read datum/dumper.sql'
 fi
-popd
+[[ $luxdir ]] && popd
