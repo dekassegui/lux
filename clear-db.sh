efc609081a503600e427eabe90ab12005563b9d1
Antonio <dekassegui@gmail.com>
2017-10-24 15:45:15 -0200

    Upgrade do db LUX.

--- clear-db.sh
+++ clear-db.sh
@@ -2,13 +2,13 @@
 #
 # Manutenção do DB do LUX
 #
-alias speech='spd-say -l pt-BR -t child_male -p 40 -r -70'
+#alias speech='spd-say -l pt-BR -t child_male -p 40 -r -70'
 # pesquisa diretório do LUX capturando resultado do comando "find"
 luxdir=$(find /home/$USER -type d -user $USER -perm -0755 -name lux -print)
 # checa se resultado é string vazia
 if [[ -z $luxdir ]]; then
   echo 'Não localizou o diretório do LUX.';
-  speech 'ops! não achei o lux'
+  #speech 'ops! não achei o lux'
   exit 1
 fi
 # muda para o diretório do LUX se necessário
@@ -34,8 +34,10 @@ if [[ $(sqlite3 $dbfile "$sql") == 1 ]]; then
   zip -m -q db-backups.zip ${entries[@]}
   # exclusão dos registros de empréstimos com data de devolucao preenchida
   echo 'n43maria' | sudo -S sqlite3 $dbfile '.read datum/dumper.sql'
-  speech 'DB ajustado'
+  echo 'DB ajustado'
+  #speech 'DB ajustado'
 else
-  speech 'atualização desnecessaria'
+  echo 'atualização desnecessaria'
+  #speech 'atualização desnecessaria'
 fi
 [[ $luxdir ]] && popd
