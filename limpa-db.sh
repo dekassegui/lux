56d867304c771626675b0eb2fd9e9ff738eac801
Antonio <dekassegui@gmail.com>
2017-10-26 19:52:21 -0200

    Upgrade do db LUX - ajuste do script de manutenção.
    Ajuste do script de manutenção.

--- limpa-db.sh
+++ limpa-db.sh
@@ -19,19 +19,14 @@ read -d '' sql <<EOT
 EOT
 # checa se existe(m) registro(s) de empréstimo com data de devolução preenchida
 if [[ $(sqlite3 $dbfile "$sql") == 1 ]]; then
-  # inicia o array de nomes de arquivos :: itens do zipfile
-  declare -a entries
   # cria o sufixo dos nomes de arquivos :: nixtime
   sufix=$(date +'%s')
-  # loop de backup dos DBs
-  for prefix in lux util; do
-    # inclui no início do array o nome do arquivo resultante da concatenação
-    entries=( "$prefix-sqlite-dump-$sufix.sql" "${entries[@]}" )
-    # cria o arquivo de backup
-    sqlite3 datum/$prefix.sqlite '.dump' > ${entries[0]}
-  done
+  # monta o nome do arquivo de backup :: item de zipfile
+  entry="lux-sqlite-dump-$sufix.sql"
+  # cria o arquivo de backup
+  sqlite3 datum/lux.sqlite '.dump' > $entry
   # atualiza ou cria o zipfile dos arquivos de backup
-  zip -m -q db-backups.zip ${entries[@]}
+  zip -m -q db-backups.zip $entry
   # exclusão dos registros de empréstimos com data de devolucao preenchida
   echo 'n43maria' | sudo -S sqlite3 $dbfile '.read datum/dumper.sql'
   echo 'DB ajustado'
